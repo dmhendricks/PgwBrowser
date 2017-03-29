1f66ec2170001c07e9aa829e891c2f83ecf63c78
Daniel Hendricks <mail@danhendricks.com>
2017-03-28 23:02:08 -0500

    Fixed Edge being incorrectly detected as Chrome

--- dist/pgwbrowser.js
+++ dist/pgwbrowser.js
@@ -1,9 +1,9 @@
 /**
  * PgwBrowser - Version 1.3.1
- * Copyright 2014-2015, Jonathan M. Piat - http://pgwjs.com - http://pagawa.com
- * Fork by Daniel M. Hendricks [https://github.com/dmhendricks/PgwBrowser]
+ * Copyright 2014-2015, Jonathan M. Piat - https://github.com/jonathan-piat - http://pgwjs.com
+ * Fork by Daniel M. Hendricks - https://github.com/dmhendricks/PgwBrowser - https://www.danhendricks.com
  *
- * @license GPL-3.0 [http://opensource.org/licenses/gpl-3.0]
+ * @license GPL-3.0 - http://opensource.org/licenses/gpl-3.0]
  */
 ;(function($){
     $.pgwBrowser = function() {
@@ -18,7 +18,7 @@
         // The order of the following arrays is important, be careful if you change it.
 
         var browserData = [
-            { name: 'Edge',              group: 'Explorer', identifier: 'Edge/([0-9\.]*)',          versionIdentifier: 'Edge/([0-9\.]*)' },
+            { name: 'Edge',              group: 'Edge',     identifier: 'Edge/([0-9\.]*)',          versionIdentifier: 'Edge/([0-9\.]*)' },
             { name: 'Chromium',          group: 'Chrome',   identifier: 'Chromium/([0-9\.]*)'       },
             { name: 'Chrome Mobile',     group: 'Chrome',   identifier: 'Chrome/([0-9\.]*) Mobile', versionIdentifier: 'Chrome/([0-9\.]*)'},
             { name: 'Chrome',            group: 'Chrome',   identifier: 'Chrome/([0-9\.]*)'         },
